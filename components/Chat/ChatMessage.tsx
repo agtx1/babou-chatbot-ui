79f58fdc43e9c38b3b6d882bf1357d7ca345828c
agtx1 <132729931+agtx1@users.noreply.github.com>
2023-07-07 17:50:17 -0500

    babou

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -22,6 +22,7 @@ import { MemoizedReactMarkdown } from '../Markdown/MemoizedReactMarkdown';
 import rehypeMathjax from 'rehype-mathjax';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
+import { Babou } from './Babou';
 
 export interface Props {
   message: Message;
@@ -136,9 +137,9 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
       <div className="relative m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] text-right font-bold">
           {message.role === 'assistant' ? (
-            <IconRobot size={30} />
+            <Babou size={30} />
           ) : (
-            <IconUser size={30} />
+            <Babou size={30} />
           )}
         </div>
 
