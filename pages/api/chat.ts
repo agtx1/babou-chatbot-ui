b899dc97a995503880f48d7feb43d11c7ee991b6
agtx1 <132729931+agtx1@users.noreply.github.com>
2023-07-11 11:12:56 -0500

    additional trim changes
    Revert "Revert "Merge branch 'main' of https://github.com/agtx1/babou-chatbot-ui""
    
    This reverts commit b83452b82488142b3a9a3fd2b5a70a15abcaf38a.
    
    try again with trim

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: 'edge',
+};
+
 import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 
@@ -8,10 +12,7 @@ import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module
 
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
 import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
-
-export const config = {
-  runtime: 'edge',
-};
+import { trim } from '@/utils/app/trim';
 
 const handler = async (req: Request): Promise<Response> => {
   try {
@@ -37,18 +38,47 @@ const handler = async (req: Request): Promise<Response> => {
     const prompt_tokens = encoding.encode(promptToSend);
 
     let tokenCount = prompt_tokens.length;
+    let totalTokenCount = messages.reduce((count, message) => count + encoding.encode(message.content).length, 0) + tokenCount;
+
     let messagesToSend: Message[] = [];
+    if (messages.length>0){
+      let lastMessage = messages.pop() as Message;
+    
+      if (1===1) {
+          // If total token count exceeds the limit, create an array of trimmed messages
+          let trimmedMessages = await Promise.all(messages.map(async message => {
+            const response = await fetch(`http://${req.headers.get('host')}/api/trim`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include', // Include cookies
+                body: JSON.stringify({ content: message.content })
+            });
+            const responseData = await response.json();
+            message.content = responseData.content;
+            return message;
+            
+        }));
 
-    for (let i = messages.length - 1; i >= 0; i--) {
-      const message = messages[i];
-      const tokens = encoding.encode(message.content);
-
-      if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
-        break;
+        trimmedMessages.push(lastMessage);
+      
+          // Start adding messages from most recent, until adding more would exceed the limit
+          for (let i = trimmedMessages.length - 1; i >= 0; i--) {
+              let tokens = encoding.encode(trimmedMessages[i].content);
+              if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
+                  break;
+              }
+              tokenCount += tokens.length;
+              messagesToSend = [trimmedMessages[i], ...messagesToSend];
+          }
+      } else {
+          // If total token count doesn't exceed the limit, use the original messages
+          messagesToSend = messages;
       }
-      tokenCount += tokens.length;
-      messagesToSend = [message, ...messagesToSend];
     }
+
+
 
     encoding.free();
 
