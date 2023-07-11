400f14641b525ecc83b6bc324fb487cdf9c7dba0
agtx1 <132729931+agtx1@users.noreply.github.com>
2023-07-11 10:15:49 -0500

    Revert "update allowDynamic"
    This reverts commit da42175691b1aa2d4f39ea2017c73e2ca9dce18e.

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,19 +1,17 @@
-export const config = {
-  runtime: 'edge',
-  unstable_allowDynamic: ['/node_modules/underscore/modules/template.js','/node_modules/underscore/modules/_setup.js']
-};
-
 import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 
 import { ChatBody, Message } from '@/types/chat';
-import {trim} from '@/utils/app/trim'
 
 // @ts-expect-error
 import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
 
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
 import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
+
+export const config = {
+  runtime: 'edge',
+};
 
 const handler = async (req: Request): Promise<Response> => {
   try {
@@ -39,31 +37,18 @@ const handler = async (req: Request): Promise<Response> => {
     const prompt_tokens = encoding.encode(promptToSend);
 
     let tokenCount = prompt_tokens.length;
-    let totalTokenCount = messages.reduce((count, message) => count + encoding.encode(message.content).length, 0) + tokenCount;
-
     let messagesToSend: Message[] = [];
-    
-    if (1===1) {
-        // If total token count exceeds the limit, create an array of trimmed messages
-        let trimmedMessages = messages.map(message => {
-            message.content = trim(message.content);
-            return message;
-        });
-    
-        // Start adding messages from most recent, until adding more would exceed the limit
-        for (let i = trimmedMessages.length - 1; i >= 0; i--) {
-            let tokens = encoding.encode(trimmedMessages[i].content);
-            if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
-                break;
-            }
-            tokenCount += tokens.length;
-            messagesToSend = [trimmedMessages[i], ...messagesToSend];
-        }
-    } else {
-        // If total token count doesn't exceed the limit, use the original messages
-        messagesToSend = messages;
+
+    for (let i = messages.length - 1; i >= 0; i--) {
+      const message = messages[i];
+      const tokens = encoding.encode(message.content);
+
+      if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
+        break;
+      }
+      tokenCount += tokens.length;
+      messagesToSend = [message, ...messagesToSend];
     }
-    
 
     encoding.free();
 
