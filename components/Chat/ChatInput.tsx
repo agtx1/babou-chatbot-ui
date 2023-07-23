218b874eada79ec95ad0fe974a3255508ea293b9
agtx1 <132729931+agtx1@users.noreply.github.com>
2023-07-23 17:20:34 -0500

    Update ChatInput.tsx to fix button click area on mobile

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -336,7 +336,7 @@ export const ChatInput = ({
           />
 
           <button
-            className="absolute right-2 top-2 rounded-sm p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200"
+            className="absolute right-2 top-2 rounded-sm p-3 md:p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200"
             onClick={handleSend}
           >
             {messageIsStreaming ? (
