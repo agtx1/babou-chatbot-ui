fe0e151121394ae1217e45f7f125d421281911ce
agtx1 <132729931+agtx1@users.noreply.github.com>
2023-11-10 14:03:12 -0600

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -12,7 +12,8 @@ export enum OpenAIModelID {
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
-  GPT_3_5_16K = 'gpt-3.5-turbo-16k'
+  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
+  GPT_4_TURBO = 'gpt-4-1106-preview'
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -48,5 +49,11 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-4-32K',
     maxLength: 96000,
     tokenLimit: 32000,
+  },
+  [OpenAIModelID.GPT_4_TURBO]: {
+    id: OpenAIModelID.GPT_4_TURBO,
+    name: 'gpt-4-1106-preview',
+    maxLength: 110000,
+    tokenLimit: 128000,
   },
 };
