970fdc66c11b2b30b17029a1484f0a2e279e3214
agtx1 <132729931+agtx1@users.noreply.github.com>
2024-07-15 09:24:50 -0500

    Update openai.ts
    update models

--- types/openai.ts
+++ types/openai.ts
@@ -9,11 +9,11 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
-  GPT_4_TURBO = 'gpt-4-0125-preview',
-  CLAUDE = 'claude-3-opus-20240229'
+//  GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_4 = 'gpt-4o',
+//  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
+//  GPT_4_TURBO = 'gpt-4-0125-preview',
+  CLAUDE = 'claude-3-5-sonnet-20240620'
 }
 
 export enum AIProviderID{
@@ -22,35 +22,14 @@ export enum AIProviderID{
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.CLAUDE;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-    provider: AIProviderID.OPENAI,
-  },
-  [OpenAIModelID.GPT_3_5_16K]: {
-    id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5-16K',
-    maxLength: 48000,
-    tokenLimit: 16000,
-    provider: AIProviderID.OPENAI,
-  },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
+    name: 'GPT-4o',
     maxLength: 24000,
     tokenLimit: 5000,
-    provider: AIProviderID.OPENAI,
-  },
-  [OpenAIModelID.GPT_4_TURBO]: {
-    id: OpenAIModelID.GPT_4_TURBO,
-    name: 'gpt-4-0125-preview',
-    maxLength: 110000,
-    tokenLimit: 128000,
     provider: AIProviderID.OPENAI,
   },
   [OpenAIModelID.CLAUDE]: {
